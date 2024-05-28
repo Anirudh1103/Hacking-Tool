915b0b06e35c107bf179151e5a63a30ebac2e669
Anirudh C M <161741952+Anirudh1103@users.noreply.github.com>
2024-05-28 20:38:30 +0530

    Delete Mini Hacking tool directory

--- Mini Hacking tool/pass_gen.py
+++
@@ -1,7 +0,0 @@
-import random
-from termcolor import colored
-password = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&+-"
-len = int(input(colored('Enter the length of the password:  ','green')))
-a = "".join(random.sample(password,len))
-print(colored('Your pass is: ','green'),end="")
-print(colored(a,'red'))
